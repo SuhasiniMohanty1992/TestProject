4141d6432ac0c363e09eb8b71accb30a7edf9a48
SatyaMallick1991 <90056984+SatyaMallick1991@users.noreply.github.com>
2021-09-04 10:54:42 +0530

    Hi

--- FirstProject/src/com/firstProject/Hello.java
+++ FirstProject/src/com/firstProject/Hello.java
@@ -2,7 +2,10 @@ package com.firstProject;
 
 public class Hello {
    private void SYso() {
-	// TODO Auto-generated method stub
+	// TODO Auto-generated method tub
 
+	   
+	   
+	   System.out.print("hii");
 }
 }
